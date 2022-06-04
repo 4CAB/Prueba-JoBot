a8b16b38e4a6a6407e422e9ab97a4f38c204b951
4CAB <trustmagali@gmail.com>
2022-06-03 19:55:21 -0500

    Fixed bug, changed webhook url

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                            port=PORT,
                            url_path=TOKEN,
-                           webhook_url="https://<appname>.herokuapp.com/" + TOKEN)
+                           webhook_url="https://webjook.herokuapp.com/" + TOKEN)
     updater.idle()
 
 if __name__ == '__main__':
